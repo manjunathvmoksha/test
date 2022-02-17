7eda6c2ae7bb9fc8adb188ce22088ece191e6341
manjunathvmoksha <manjunath.nk@vmokshagroup.com>
2022-02-17 17:37:00 +0530

    first commit

---
+++ app8/app/Http/Controllers/VideoRecordingController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use ProtoneMidea\laravelFFMpeg\support\FFMpeg;
+
+class VideoRecordingController extends Controller
+{
+    //
+    public function index(){
+        // $submitText = NULL;
+        return view('./');
+
+        // return view('video');
+    }
+
+    public function store(){
+        $file = tap($request->file('video'))->store('videos');
+        $filename = pathinfo($file->hasName(), PATHINFO_FILENAME);
+
+        dd($filename);
+        FFMpeg::fromDisk("Local")
+        ->open("videos/".$file->hasName())
+        ->export()
+        ->toDisk()("Local")
+        ->inFormat(new \FFMpeg\Format\Video\x264('libm3lane', 'libx264'))
+        ->save('converted_videos/'.$filename.'.mp4');
+        
+        return response()->json([
+            'status'=> 'success'
+        ]);
+    }
+
+    public function __invoke(Request $request)
+    {
+        FFMpeg::open($request->file('video'));
+    }
+}
